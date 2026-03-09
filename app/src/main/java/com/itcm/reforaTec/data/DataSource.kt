7aec40dea44607857a4d3d5ef771bd2a2fccfeed
Chilvers Del Angel <l21070316@cdmadero.tecnm.mx>
2026-03-08 19:07:32 -0600

    feat: implement DataSource and resources for tree list

---
+++ app/src/main/java/com/itcm/reforaTec/data/DataSource.kt
@@ -0,0 +1,25 @@
+package com.itcm.reforaTec.data
+
+import com.itcm.reforaTec.R
+import com.itcm.reforaTec.model.Tree
+
+class DataSource {
+
+    fun loadTrees(): List<Tree> {
+        return listOf<Tree>(
+            Tree(
+                value = R.string.first_tree_value,
+                commonName = R.string.first_tree_common_name,
+                scientificName = R.string.first_tree_scientific_name,
+                image = R.drawable.first_tree_image
+            ),
+
+            Tree(
+                value = R.string.second_tree_value,
+                commonName = R.string.second_tree_common_name,
+                scientificName = R.string.second_tree_scientific_name,
+                image = R.drawable.second_tree_image
+            )
+        )
+    }
+}
