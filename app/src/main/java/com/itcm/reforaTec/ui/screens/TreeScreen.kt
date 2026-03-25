0f5a9f3a805c293fd8aa994300da12e6e9195271
Chilvers Del Angel <l21070316@cdmadero.tecnm.mx>
2026-03-25 10:28:21 -0600

    refactor: apply state hoisting with TreeState object and clean UI padding

--- app/src/main/java/com/itcm/reforaTec/ui/screens/TreeScreen.kt
+++ app/src/main/java/com/itcm/reforaTec/ui/screens/TreeScreen.kt
@@ -10,49 +10,69 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.itcm.reforaTec.data.DataSource
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.itcm.reforaTec.model.Tree
+import com.itcm.reforaTec.ui.viewmodels.TreeState
+import com.itcm.reforaTec.ui.viewmodels.TreeViewModel
 
 @Composable
-fun TreeScreen() {
-    val dataSource = DataSource()
-    val trees: List<Tree> = dataSource.loadTrees()
+fun TreeScreen(
+    viewModel: TreeViewModel = viewModel()
+) {
+    val scrollState = rememberScrollState()
 
-    var servicesCount by remember { mutableIntStateOf(value = 0) }
-    var historyCount by remember { mutableIntStateOf(value = 0) }
-
-    Scaffold(topBar = { TreeTopAppBar() }, bottomBar = { TreeBottomAppBar() }) { innerPadding ->
+    Scaffold(
+        topBar = { TreeTopAppBar() },
+        bottomBar = { TreeBottomAppBar() })
+    { innerPadding ->
         Column(
-            modifier = Modifier.padding(paddingValues = innerPadding),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier
+                .padding(paddingValues = innerPadding)
+                .verticalScroll(scrollState)
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
+            viewModel.trees.forEach { tree ->
+                val state = viewModel.getTreeState(tree)
 
-            trees.forEach { tree ->
                 TreeCard(
                     tree = tree,
-                    modifier = Modifier.padding(16.dp),
-                    servicesCount = servicesCount,
-                    historyCount = historyCount,
-                    setServicesCount = { servicesCount++ },
-                    setHistoryCount = { historyCount++ })
+                    state = state,
+                    setServicesCount = { viewModel.incrementServices(tree) },
+                    setHistoryCount = { viewModel.incrementHistory(tree) },
+                    onValueChange = { newValue ->
+                        viewModel.updateCustomValue(
+                            tree, newValue
+                        ) },
+                    onCommonNameChange = { newName ->
+                        viewModel.updateCustomCommonName(
+                            tree, newName
+                        ) },
+                )
+
+                Text("Valor: ${state.customValue}")
+                Text("Nombre común: ${state.customCommonName}")
+                Text("Contador de servicios: ${state.servicesCount}")
+                Text("Contador de historial: ${state.historyCount}")
             }
         }
     }
@@ -60,15 +80,12 @@ fun TreeScreen() {
 
 @Composable
 fun TreeTopAppBar() {
-    TopAppBar(
-        title = { Text("Especie") })
+    TopAppBar(title = { Text("Especie") })
 }
 
 @Composable
 fun TreeBottomAppBar() {
-    BottomAppBar(
-        Modifier.padding(16.dp)
-    ) {
+    BottomAppBar(Modifier.padding(16.dp)) {
         Text("BottomAppBar de demostración.")
     }
 }
@@ -77,51 +94,71 @@ fun TreeBottomAppBar() {
 fun TreeCard(
     tree: Tree,
     modifier: Modifier = Modifier,
-    servicesCount: Int,
-    historyCount: Int,
+    state: TreeState,
     setServicesCount: () -> Unit,
-    setHistoryCount: () -> Unit
+    setHistoryCount: () -> Unit,
+    onValueChange: (String) -> Unit,
+    onCommonNameChange: (String) -> Unit
 ) {
-    Card(
-        modifier = modifier
-    ) {
-        Column(
-            Modifier.padding(16.dp)
-        ) {
-            Text(
-                "Nombre común: ${stringResource(id = tree.commonName)}", style = MaterialTheme.typography.headlineSmall
-            )
-            Spacer(modifier = Modifier.height(8.dp))
+    Card(modifier = modifier) {
+        Column(Modifier.padding(16.dp)) {
 
-            Text("Nombre científico: ${stringResource(id = tree.scientificName)}")
-            Spacer(modifier = Modifier.height(8.dp))
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painterResource(id = tree.image),
+                    contentDescription = "Imagen del árbol.",
+                    modifier = Modifier
+                        .size(80.dp)
+                        .padding(end = 16.dp),
+                    contentScale = ContentScale.Crop
+                )
 
-            Image(
-                painterResource(id = tree.image),
-                contentDescription = "Imagen del árbol.",
-                modifier = Modifier.fillMaxWidth()
-            )
+                Column {
+                    val currentValue = state.customValue ?: stringResource(tree.value)
+                    val currentCommonName = state.customCommonName ?: stringResource(tree.commonName)
 
-            Row(
-                modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
-            ) {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Button(onClick = setServicesCount) {
-                        Text("Servicios")
-                    }
-                    Text(servicesCount.toString())
+                    OutlinedTextField(
+                        value = currentValue,
+                        onValueChange = onValueChange,
+                        label = { Text("Valor:") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    OutlinedTextField(
+                        value = currentCommonName,
+                        onValueChange = onCommonNameChange,
+                        label = { Text("Nombre común:") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    Text("Nombre científico:")
+                    Text(stringResource(id = tree.scientificName))
+                    Spacer(modifier = Modifier.height(8.dp))
                 }
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Button(onClick = setHistoryCount) {
-                        Text("Historial")
-                    }
-                    Text(historyCount.toString())
-                }
+
             }
+
+            TreeCounters(state.servicesCount, state.historyCount, setServicesCount, setHistoryCount)
         }
+    }
+}
+
+@Composable
+fun TreeCounters(
+    servicesCount: Int, historyCount: Int, setServicesCount: () -> Unit, setHistoryCount: () -> Unit
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = setServicesCount) { Text("Servicios") }
+            // Text(servicesCount.toString())
+        }
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Button(onClick = setHistoryCount) { Text("Historial") }
+            // Text(historyCount.toString())
+        }
+
+        Button({}) { Text("Más") }
     }
 }
