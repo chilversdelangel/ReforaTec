1ec3c530e79656337b79a51df9a67f78d1dc67c7
Chilvers Del Angel <l21070316@cdmadero.tecnm.mx>
2026-03-25 15:29:01 -0600

    feat: add id to Tree model and expand mock data

--- app/src/main/java/com/itcm/reforaTec/data/DataSource.kt
+++ app/src/main/java/com/itcm/reforaTec/data/DataSource.kt
@@ -8,6 +8,7 @@ class DataSource {
     fun loadTrees(): List<Tree> {
         return listOf(
             Tree(
+                id = R.string.first_tree_id,
                 value = R.string.first_tree_value,
                 commonName = R.string.first_tree_common_name,
                 scientificName = R.string.first_tree_scientific_name,
@@ -15,10 +16,35 @@ class DataSource {
             ),
 
             Tree(
+                id = R.string.second_tree_id,
                 value = R.string.second_tree_value,
                 commonName = R.string.second_tree_common_name,
                 scientificName = R.string.second_tree_scientific_name,
                 image = R.drawable.second_tree_image
+            ),
+
+            Tree(
+                id = R.string.third_tree_id,
+                value = R.string.third_tree_value,
+                commonName = R.string.third_tree_common_name,
+                scientificName = R.string.third_tree_scientific_name,
+                image = R.drawable.first_tree_image
+            ),
+
+            Tree(
+                id = R.string.fourth_tree_id,
+                value = R.string.fourth_tree_value,
+                commonName = R.string.fourth_tree_common_name,
+                scientificName = R.string.fourth_tree_scientific_name,
+                image = R.drawable.second_tree_image
+            ),
+
+            Tree(
+                id = R.string.fifth_tree_id,
+                value = R.string.fifth_tree_value,
+                commonName = R.string.fifth_tree_common_name,
+                scientificName = R.string.fifth_tree_scientific_name,
+                image = R.drawable.first_tree_image
             )
         )
     }
--- app/src/main/java/com/itcm/reforaTec/model/Tree.kt
+++ app/src/main/java/com/itcm/reforaTec/model/Tree.kt
@@ -4,6 +4,7 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
 data class Tree(
+    @StringRes val id: Int,
     @StringRes val value: Int,
     @StringRes val commonName: Int,
     @StringRes val scientificName: Int,
