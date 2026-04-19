c4af8530262d0c5295b42f9762ec1922c014bc55
Chilvers Del Angel <l21070316@cdmadero.tecnm.mx>
2026-04-19 17:04:12 -0600

    feat(ui): define trees UiState for reactive updates

---
+++ app/src/main/java/com/itcm/reforaTec/ui/TreesUiState.kt
@@ -0,0 +1,18 @@
+package com.itcm.reforaTec.ui
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class TreeState(
+    val id: String,
+    val imageUri: String,
+    val commonName: String,
+    val scientificName: String,
+    val value: String,
+    val servicesCount: Int = 0,
+    val historyCount: Int = 0,
+)
+
+data class TreesUiState(
+    val treeStates: List<TreeState> = emptyList()
+)
