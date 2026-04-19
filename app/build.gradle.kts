b218701eef763167f487a48056de23c92c5839e5
Chilvers Del Angel <l21070316@cdmadero.tecnm.mx>
2026-04-19 17:01:19 -0600

    feat(android): add internet permission and coil 3.4.0 dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation("io.coil-kt.coil3:coil-compose:3.4.0")
+    implementation("io.coil-kt.coil3:coil-network-okhttp:3.4.0")
     implementation(libs.androidx.ui.text.google.fonts)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
